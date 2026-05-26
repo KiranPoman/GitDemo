a8db4f56d7a84ac6733a3bfb3d30f2718cc2abb1
Apps-PeCfy <49563771+Apps-PeCfy@users.noreply.github.com>
2026-05-26 18:26:59 +0530

    push new branch test

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -26,7 +26,6 @@ public class LoginPage {
 
 	
 	public ProductPage loginApplication(String emailId, String pass) {
-		System.out.println("Login with email: " + emailId + " and password: " + pass);
 		email.sendKeys(emailId);
 		password.sendKeys(pass);
 		loginBtn.click();
@@ -38,6 +37,7 @@ public class LoginPage {
 	public void goTo() {
 		driver.get("https://rahulshettyacademy.com/client/");
 		System.out.println("Navigated to login page");
+		System.out.println("Create New Branch on GitHub");
 		
 	}	
 
