ddf964e463436e10024da2b6c5bae69960426b2d
Apps-PeCfy <49563771+Apps-PeCfy@users.noreply.github.com>
2026-06-10 19:32:39 +0530

    CICD

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -39,7 +39,7 @@ public class LoginPage {
 		System.out.println("Create New Branch on GitHub and push to new branch");
 		
 	System.out.println("Create New Branch ");
-	System.out.println("CICDW ");
+	System.out.println("CICD with Jenkins ");
 		
 	}	
 
